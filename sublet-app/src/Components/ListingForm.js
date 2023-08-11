7c73970560d70a344ab2dd34c464ae9489300c4b
parths10 <parth.sehtiyaa@gmail.com>
2023-08-11 13:02:53 -0700

    code refactored and cleaned

--- sublet-app/src/Components/ListingForm.js
+++ sublet-app/src/Components/ListingForm.js
@@ -1,80 +1,3 @@
-// import React, {useState} from 'react';
-// import './ListingForm.css';
-// import PopupForm from './PopupForm';
-// import {connect} from 'react-redux';
-// import { addListing } from '../Redux/actions';
-// import axios from 'axios';
-//
-// function ListingForm({ onAddListing }) {
-//     const [formData, setFormData] = useState({
-//         name: '',
-//         contact: '',
-//         residenceArea: '',
-//         roomType: '',
-//         expectedRent: '',
-//         description: '',
-//         image: null,
-//     });
-//
-//     const [showPopup, setShowPopup] = useState(false);
-//
-//
-//     const handleChange = (e) => {
-//         const { name, value } = e.target;
-//         setFormData({ ...formData, [name]: value });
-//     };
-//
-//     const handleImageChange = (e) => {
-//         setFormData({ ...formData, image: e.target.files[0] });
-//     };
-//
-//
-//     const postNew = (newPost) => {
-//         console.log('item added');
-//         axios.post('http://localhost:4555/insert', { name: formData.name, contact: formData.contact, residenceArea: formData.residenceArea, roomType: formData.roomType, expectedRent: formData.expectedRent, description: formData.description, image: null});
-//     };
-//
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         onAddListing(formData);
-//         setFormData({
-//             name: '',
-//             contact: '',
-//             residenceArea: '',
-//             roomType: '',
-//             expectedRent: '',
-//             description: '',
-//             image: null,
-//         });
-//
-//         const newPost = {
-//             name, contact, residenceArea, roomType, expectedRent, description, image
-//         };
-//
-//         postNew(newPost);
-//
-//         setShowPopup(false);
-//     };
-//
-//     return (
-//         <div className="ListingForm">
-//             {showPopup && (
-//                 <PopupForm
-//                     formData={formData}
-//                     handleChange={handleChange}
-//                     handleImageChange={handleImageChange}
-//                     handleSubmit={handleSubmit}
-//                     onClose={() => setShowPopup(false)}
-//                 />
-//             )}
-//             <button onClick={() => setShowPopup(true)}>Post</button>
-//         </div>
-//     );
-// }
-//
-// export default connect(null, { onAddListing: addListing })(ListingForm);
-//
-
 
 import React, { useState } from 'react';
 import './ListingForm.css';
@@ -91,7 +14,7 @@ function ListingForm({ onAddListing }) {
         roomType: '',
         expectedRent: '',
         description: '',
-        image: null,
+        image: '',
     });
 
     const [showPopup, setShowPopup] = useState(false);
@@ -122,7 +45,7 @@ function ListingForm({ onAddListing }) {
                 roomType: '',
                 expectedRent: '',
                 description: '',
-                image: null,
+                image: '',
             });
 
             setShowPopup(false);
