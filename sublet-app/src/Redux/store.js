5f3549dcb68e11d130ae511765510774383fa1e6
Parth Sehtiya <64464531+parths10@users.noreply.github.com>
2023-08-14 16:11:56 -0700

    Update store.js

--- sublet-app/src/Redux/store.js
+++ sublet-app/src/Redux/store.js
@@ -8,15 +8,4 @@ export default store;
 
 
 
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import listingsReducer from './reducers';
-//
-// // Use the Redux DevTools Extension if available, otherwise use applyMiddleware
-// const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-//
-// const store = createStore(
-//     listingsReducer,
-//     composeEnhancers(applyMiddleware())  // No middleware added for now, but this setup allows for future addition
-// );
-//
-// export default store;
+
