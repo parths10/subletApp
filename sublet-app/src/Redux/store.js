56e94a68a4cfff3ce82772115325fda740ec36b4
Parth Sehtiya <64464531+parths10@users.noreply.github.com>
2023-08-14 16:29:30 -0700

    Update store.js

--- sublet-app/src/Redux/store.js
+++ sublet-app/src/Redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
 import listingsReducer from "./reducers";
 
+// stores the state using listingReducer
 const store = createStore(listingsReducer);
 
 export default store;
